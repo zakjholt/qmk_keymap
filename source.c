1644965583a136769fa36b192179525d776537c6
Zak Holt <zak.j.holt@gmail.com>
2023-01-05 09:00:06 -0500

    remove custom code

--- source.c
+++ source.c
@@ -1,10 +1,1 @@
 #include QMK_KEYBOARD_H
-
-enum custom_keycodes { SWAP = SAFE_RANGE };
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-  default:
-    return true;
-  }
-}
